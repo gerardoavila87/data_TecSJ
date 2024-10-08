015086a6a7e29ac0a29cf4ad2bdf7441aec386d6
Gerardo Ávila <gerardo.avila@tecmm.edu.mx>
2024-10-08 12:28:27 -0600

    Delete dist directory

--- dist/routes/UnidadRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const UnidadController_1 = require("../controllers/UnidadController");
-const router = (0, express_1.Router)();
-router.get('/unidades/Oficial', UnidadController_1.getAllUnidadO);
-router.get('/Unidades/Real', UnidadController_1.getAllUnidadR);
-exports.default = router;
